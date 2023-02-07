8269052c398a56be0df697b2490d858b698509a0
Noah <15622274882@163.com>
2023-02-07 15:47:44 +0800

    chore: updating configuration file

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,48 +39,7 @@ module.exports = {
   overrides: [
     {
       files: ['packages/*/components/**/*.vue', 'docs/**/*.vue'],
-      env: {
-        browser: true,
-        node: true,
-        es2022: true,
-      },
-      parser: 'vue-eslint-parser',
-      parserOptions: {
-        parser: '@typescript-eslint/parser',
-        sourceType: 'module',
-      },
-      extends: [
-        'eslint:recommended',
-        'plugin:vue/vue3-essential',
-        'plugin:vue/vue3-strongly-recommended',
-        'plugin:vue/vue3-recommended',
-        'plugin:prettier/recommended',
-        'prettier',
-      ],
-      plugins: ['vue', '@typescript-eslint'],
-      rules: {
-        'vue/html-self-closing': [
-          'error',
-          {
-            html: {
-              void: 'always',
-              normal: 'never',
-              component: 'always',
-            },
-            svg: 'always',
-            math: 'always',
-          },
-        ],
-        'vue/max-attributes-per-line': 'off',
-        'vue/singleline-html-element-content-newline': 'off',
-        'vue/no-v-html': 'off',
-        'vue/multi-word-component-names': 'off',
-        'no-unused-vars': 'off',
-        'no-console': 'off',
-        'no-undef': 'off',
-        '@typescript-eslint/no-unused-vars': 'error',
-        '@typescript-eslint/no-explicit-any': 'error',
-      },
+      extends: ['@noahyu/vue'],
     },
     {
       files: ['docs/**/*.vue'],
