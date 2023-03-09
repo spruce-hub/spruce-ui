9e7d742a79531f364819b68176f2c28fdd88b059
Noah <noah4882@163.com>
2023-03-09 19:35:16 +0800

    chore: update config

--- preview/vite.config.ts
+++ preview/vite.config.ts
@@ -1,14 +1,15 @@
 import { resolve } from 'node:path'
 import { cwd } from 'node:process'
 
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 const cui = resolve(cwd(), '../packages/cui')
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue()],
+  plugins: [vue(), visualizer() as PluginOption],
   resolve: {
     alias: {
       '@': '/src',
