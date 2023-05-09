aea912e26bfef3d0e9030cedebdc4b271c272938
Noah <noah4882@163.com>
2023-05-09 14:13:03 +0800

    chore: update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
   ],
   plugins: ['@typescript-eslint'],
   rules: {
-    indent: ['error', 2],
+    indent: ['error', 2, { SwitchCase: 1 }],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'never'],
