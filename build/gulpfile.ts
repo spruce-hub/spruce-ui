c2fc4c8af38a8ef1d762bd87bd1533549a4a9a7f
Noah <noah4882@163.com>
2023-05-09 11:58:27 +0800

    build: 为任意一个包提供单独打包的能力 (#7)
    例：pnpm build --lib cui

--- build/gulpfile.ts
+++ build/gulpfile.ts
@@ -1,3 +1,4 @@
+import { argv } from 'node:process'
 import gulp from 'gulp'
 
 import {
@@ -16,16 +17,40 @@ import {
 
 const { series } = gulp
 
-export default series(
-  buildHooks,
-  buildIcons,
-  buildCui,
-  buildCuiStyle,
-  copyCuiScss,
-  buildEui,
-  buildEuiStyle,
-  copyEuiScss,
-  buildMui,
-  buildMuiStyle,
-  copyMuiScss
-)
+let tasks
+
+const libNameIndex = argv.indexOf('--lib') + 1
+
+switch (argv[libNameIndex]) {
+  case 'cui':
+    tasks = series(buildCui, buildCuiStyle, copyCuiScss)
+    break
+  case 'eui':
+    tasks = series(buildEui, buildEuiStyle, copyEuiScss)
+    break
+  case 'mui':
+    tasks = series(buildMui, buildMuiStyle, copyMuiScss)
+    break
+  case 'hooks':
+    tasks = series(buildHooks)
+    break
+  case 'icons':
+    tasks = series(buildIcons)
+    break
+  default:
+    tasks = series(
+      buildHooks,
+      buildIcons,
+      buildCui,
+      buildCuiStyle,
+      copyCuiScss,
+      buildEui,
+      buildEuiStyle,
+      copyEuiScss,
+      buildMui,
+      buildMuiStyle,
+      copyMuiScss
+    )
+}
+
+export default tasks
