8b490e9cd66e8fe36201b4d6fc3a35e2b2342bf4
Noah <noah4882@163.com>
2023-08-15 20:41:13 +0800

    feat(component): 默认导入 css，无需手动导入

--- packages/eui/components/header-menu/index.ts
+++ packages/eui/components/header-menu/index.ts
@@ -1,5 +1,6 @@
 import type { App, Plugin } from 'vue'
 import HeaderMenu from './src/header-menu.vue'
+import '@eui/components/header-menu/style/css'
 
 export const HeaderMenuPlugin: Plugin = {
   install(app: App) {
